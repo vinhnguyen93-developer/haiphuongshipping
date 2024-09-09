a810e9838786af3033ccdd68090a8189c736cb0d
Zhang Duy <truongkhanhduy1004@gmail.com>
2024-09-09 14:33:35 +0700

    update footer

--- wp-content/themes/haiphuong/footer.php
+++ wp-content/themes/haiphuong/footer.php
@@ -34,11 +34,13 @@
             </svg>  
             Email: Global@haiphuongshipping.com</p>
           </div>
-          <div class="title-size d-flex">
+          <div class="title-size d-flex align-items-center">
+          <img src="<?php bloginfo('template_url'); ?>/assets/phone.png" alt=""/>
             <p class="text-white">
-            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+            
+            <!-- <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
-            </svg>  
+            </svg>   -->
             Điện Thoại: 0916.114.888</p>
           </div>
         </div>
