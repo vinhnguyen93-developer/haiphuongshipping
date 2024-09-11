45f6aa5e2d0a1d9ac51063da2c847e9d03f49c6b
VinhNguyen <vinh@cck.co.jp>
2024-09-11 09:41:46 +0700

    [Update] section about us and service

--- wp-content/themes/haiphuong/index.php
+++ wp-content/themes/haiphuong/index.php
@@ -57,4 +57,66 @@
   </div>
 </section>
 
+<section class="container py-4 about-us">
+  <div class="row gx-3 gy-md-0 gy-4 align-items-end">
+    <div class="col-12 col-md-6">
+      <div>
+        <img class="about-us-image" src="<?php bloginfo('template_url'); ?>/assets/hp-office.jpg" alt="Hải Phương Office">
+      </div>
+    </div>
+    <div class="col-12 col-md-6">
+      <div class="text-center text-md-start px-3 px-md-0">
+        <h2 class="section-title mb-5 about-us-title">Về chúng tôi</h2>
+        <h3 class="about-us-sub-title fw-semibold mb-3 lh-base lh-md-lg text-start">
+          Thành lập vào 31 tháng 5 năm 2018, Công ty TNHH vận tải biển Hải Phương (Hai Phuong Shipping Co., Ltd - HPS) rà đời với sứ mệnh phục vụ vận chuyển hàng hóa nội thủy và vận tải biển quốc tế với dịch vụ tốt nhất, đưa ra những giải pháp hữu ích nhất cho khách hàng.
+        </h3>
+        <p class="fw-light lh-base text-start">
+          Với phương châm "Sharing Experience, Serving Excellence!" - "Chia sẻ hết mình, Phục vụ tận tình!" đội ngũ HPS luôn nỗ lực trau dồi, ý thức sứ mệnh và cam kết hỗ trợ, đáp ứng mọi nhu cầu của khách hàng ngày càng tốt hơn.
+        </p>
+
+        <a class="btn mt-5 text-white about-us-btn rounded-0 btn-lg" href="#">Liên hệ ngay</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="pt-3">
+  <div class="d-flex justify-content-center mb-5">
+    <h2 class="section-title">Dịch vụ cung cấp</h2>
+  </div>
+
+  <div class="service-body">
+    <div class="">
+      <div>
+        <h2>01</h2>
+        <h2>Vận tải hàng hóa trọn gói bằng đường biển</h2>
+        <p>Chúng tôi tự tin có thể cung cấp</p>
+        <i class="fa-solid fa-angles-right"></i>
+      </div>
+      <div>
+        <h2>02</h2>
+        <h2>Vận tải hàng hóa trọn gói bằng đường biển</h2>
+        <i class="fa-solid fa-angles-right"></i>
+      </div>
+      <div>
+        <h2>03</h2>
+        <h2>Vận tải hàng hóa trọn gói bằng đường biển</h2>
+        <i class="fa-solid fa-angles-right"></i>
+      </div>
+      <div>
+        <h2>04</h2>
+        <h2>Vận tải hàng hóa trọn gói bằng đường biển</h2>
+        <i class="fa-solid fa-angles-right"></i>
+      </div>
+      <div>
+        <h2>05</h2>
+        <h2>Vận tải hàng hóa trọn gói bằng đường biển</h2>
+        <i class="fa-solid fa-angles-right"></i>
+      </div>
+    </div>
+
+    <div class="service-layer"></div>
+  </div>
+</section>
+
 <?php get_footer(); ?>
