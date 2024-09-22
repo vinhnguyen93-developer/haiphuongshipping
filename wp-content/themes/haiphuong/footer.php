d400ac39599587c4561e97da8478ff1cf214ad51
Zhang Duy <truongkhanhduy1004@gmail.com>
2024-09-23 00:16:49 +0700

    update footer

--- wp-content/themes/haiphuong/footer.php
+++ wp-content/themes/haiphuong/footer.php
@@ -3,62 +3,98 @@
 <footer id="footer" role="contentinfo">
 <div class="footer py-5 ">
   
-  <div class="contact d-flex justify-content-around flex-column flex-md-row">
-    <div class="map w-100">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3135.5260989453227!2d106.71176264641367!3d20.840187032604582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7aaf9bd6d537%3A0x22be8ff047aabfc3!2zMThkIMSQLiBMw6ogSOG7k25nIFBob25nLCDEkOG6sW5nIEzDom0sIEjhuqNpIEFuLCBI4bqjaSBQaMOybmcsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1725788714874!5m2!1svi!2s" width="100%" height="250px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    
-    </div>  
-    <div class="information d-flex flex-column mb-3 align-item-center">
-      <div>
-        <H1 class="footer-h1 text-white h-100">CÔNG TY TNHH VẬN TẢI BIỂN HẢI PHƯƠNG</H1>
+  <div class="contact d-flex mb-3 flex-column flex-md-row">
+    <div class="d-logo-footer p-2 d-flex justify-content-center align-items-center">
+      <div class="map d-flex justify-content-center align-items-center">
+        <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3135.5260989453227!2d106.71176264641367!3d20.840187032604582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7aaf9bd6d537%3A0x22be8ff047aabfc3!2zMThkIMSQLiBMw6ogSOG7k25nIFBob25nLCDEkOG6sW5nIEzDom0sIEjhuqNpIEFuLCBI4bqjaSBQaMOybmcsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1725788714874!5m2!1svi!2s" width="100%" height="250px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+        <img  class="logo-size"src="<?php bloginfo('template_url'); ?>/assets/logo-footer.png" alt=""/>
       </div>
-      <div class="d-flex d-flex flex-column mb-3">
-        <div class="title-size d-flex align-items-center">
-          <div class="d-flex size d-flex align-items-center">
-          <img  class="img-size"src="<?php bloginfo('template_url'); ?>/assets/gps.png" alt=""/>
-          <P class="text-white footer-p">
-          Địa chỉ giao dịch:</P>
-          </div>
-          <div>
-            <p class="text-white footer-p">Số 54 lô 18D Lê Hồng Phong, Phường Đằng Lâm, Quận Hải An, TP Hải Phòng
-            </p>
-          </div>
-        </div>
-        <div class="title-size d-flex align-items-center">
-          <div class="d-flex size d-flex align-items-center"> 
-              <img class="img-size" src="<?php bloginfo('template_url'); ?>/assets/mail.png" alt=""/>
-              <p class="text-white footer-p">
-              Email:</p>
-          </div>
-            <div>
-            <a href="mailto:global@haiphuongshipping.com" class="d-flex">
-              <p class="text-white footer-p">
-              global@haiphuongshipping.com
-              </p>
-            </a>
-            </div>
-        </div>
+    </div>
+      
+    <div class="information p-3 d-flex flex-column align-item-center justify-content-center">
         
+      <div class="d-flex d-flex flex-column mb-3 ">
+          <div>
+          <H1 class="footer-h1 text-white ">CÔNG TY TNHH VẬN TẢI BIỂN HẢI PHƯƠNG</H1>
+          </div>
           <div class="title-size d-flex align-items-center">
-            <div class="d-flex size d-flex align-items-center" >
-              <img  class="img-size"src="<?php bloginfo('template_url'); ?>/assets/phone.png" alt=""/>
-              <p class="text-white footer-p">
-              Điện Thoại:
-              </p>
+            <div class="d-flex size d-flex align-items-center">
+            <!-- <img  class="img-size"src="<?php bloginfo('template_url'); ?>/assets/gps.png" alt=""/> -->
+
+            <P class="text-white footer-p">
+            Địa chỉ giao dịch: Số 54 lô 18D Lê Hồng Phong, Phường Đằng Lâm,</P>
+            </div>
+            <div>
+              <!-- <p class="text-white footer-p">Số 54 lô 18D Lê Hồng Phong, Phường Đằng Lâm, Quận Hải An, TP Hải Phòng
+              </p> -->
+            </div>
+          </div>
+          <div class="title-size d-flex align-items-center">
+            <div class="d-flex size d-flex align-items-center">
+            <!-- <img  class="img-size"src="<?php bloginfo('template_url'); ?>/assets/gps.png" alt=""/> -->
+
+            <P class="text-white footer-p">
+            Quận Hải An, TP Hải Phòng</P>
+            </div>
+            <div>
+              <!-- <p class="text-white footer-p">Số 54 lô 18D Lê Hồng Phong, Phường Đằng Lâm, Quận Hải An, TP Hải Phòng
+              </p> -->
+            </div>
+          </div>
+          <div class="title-size d-flex align-items-center">
+            <div class="d-flex size d-flex align-items-center"> 
+                <!-- <img class="img-size" src="<?php bloginfo('template_url'); ?>/assets/mail.png" alt=""/>
+     -->
+                <p class="text-white footer-p">
+                Email:</p>
             </div>
               <div>
-              <a href="tel:+84 945.776.999" class="d-flex me-5">
+              <a href="mailto:global@haiphuongshipping.com" class="d-flex">
                 <p class="text-white footer-p">
-                  +84 945.776.999
+                global@haiphuongshipping.com
                 </p>
               </a>
               </div>
           </div>
           
+          <div class="title-size d-flex align-items-center">
+              <div class="d-flex size d-flex align-items-center" >
+                <!-- <img  class="img-size"src="<?php bloginfo('template_url'); ?>/assets/phone.png" alt=""/>
+     -->
+                <p class="text-white footer-p">
+                Điện Thoại:
+                </p>
+              </div>
+                <div>
+                <a href="tel:+84 945.776.999" class="d-flex me-5">
+                  <p class="text-white footer-p">
+                    +84 945.776.999
+                  </p>
+                </a>
+                </div>
+          </div>
+          
         </div>
-      </div>
+      
      
       
+    </div>
+    <div class="d-menu ms-auto p-2 flex-column justify-content-center align-items-end">
+      <a class="text-white footer-p" href="">
+        Về chúng tôi
+      </a>
+      <a class="d-menu-item text-white footer-p" href="">
+        Đội tàu
+      </a>
+      <a class="d-menu-item text-white footer-p" href="">
+        Dịch vụ
+      </a>
+      <a class="d-menu-item text-white footer-p" href="">
+        Tuyển dụng
+      </a>
+      <a class="d-menu-item text-white footer-p" href="">
+        Tin Tức
+      </a>
     </div>
   </div>
 
