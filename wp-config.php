1bbc07eee2dd1a4e42d8581574f099bd903c23b5
Zhang Duy <truongkhanhduy1004@gmail.com>
2024-09-24 11:44:09 +0700

    update service

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'haiphuong' );
 define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
